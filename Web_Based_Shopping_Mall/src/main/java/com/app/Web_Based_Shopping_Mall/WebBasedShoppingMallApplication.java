7836095b31c2f47ff27d9c70b82c91fadc39389a
Nadeesha-D-Shalom <nadeeshashalom1@gmail.com>
2025-09-30 16:45:26 +0530

    v 1.0.1

---
+++ Web_Based_Shopping_Mall/src/main/java/com/app/Web_Based_Shopping_Mall/WebBasedShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package com.app.Web_Based_Shopping_Mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBasedShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBasedShoppingMallApplication.class, args);
+	}
+
+}
