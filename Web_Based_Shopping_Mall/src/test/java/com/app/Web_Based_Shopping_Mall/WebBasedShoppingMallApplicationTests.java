da078a256a6920ca01aebb9f3039f11e1ce86bb5
Nadeesha-D-Shalom <nadeeshashalom1@gmail.com>
2025-10-15 22:09:43 +0530

    Version 1.1.6 - Created program files.

---
+++ Web_Based_Shopping_Mall/src/test/java/com/app/Web_Based_Shopping_Mall/WebBasedShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.Web_Based_Shopping_Mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBasedShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
